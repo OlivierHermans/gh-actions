bc6f8edd3b688e73e369a5102e496393ff3c72a6
Olivier <oli4hermans@gmail.com>
2024-11-25 16:09:35 +0100

    Another change

--- src/main/java/be/olivierhermans/Main.java
+++ src/main/java/be/olivierhermans/Main.java
@@ -3,6 +3,6 @@ package be.olivierhermans;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! This change is introduced via PR");
+        System.out.println("Hello World!");
     }
 }
